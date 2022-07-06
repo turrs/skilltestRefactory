136ea2d41233ae3c26a2e7edde19d93a52940f6e
turrs <faturrahmanavoiders@gmail.com>
2022-07-07 05:11:12 +0800

    create all pages

--- src/Assets/ilustration/index.js
+++ src/Assets/ilustration/index.js
@@ -2,4 +2,11 @@ import ILlandingpage1 from "./ilustrasi-landing-1.png";
 import ILonlineForum from "./landingGraphic.svg";
 import ILsoon from "./soon.png";
 import ILmicroblogging from "./microblogging.svg";
-export { ILlandingpage1, ILonlineForum, ILsoon, ILmicroblogging };
+import ILkokoacademy from "./koko-ademy.svg";
+export {
+  ILlandingpage1,
+  ILonlineForum,
+  ILsoon,
+  ILmicroblogging,
+  ILkokoacademy,
+};
--- src/Pages/Academy/index.js
+++ src/Pages/Academy/index.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { ILkokoacademy } from "../../Assets";
 import { Footer } from "../../Components";
 import { Header, Jumbotron } from "../../Parts";
-
+import styles from "./academy.module.css";
 export default function Academy() {
   return (
     <div>
       <Header></Header>
-      <Jumbotron></Jumbotron>
+      <div className={styles.container}>
+        <div>
+          <div>
+            <p>test</p>
+          </div>
+          <div>
+            <img src={ILkokoacademy}></img>
+          </div>
+        </div>
+      </div>
       <Footer></Footer>
     </div>
   );
